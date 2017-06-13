8e7191016bb9fda72e4e62f5e94a5bc10732a9c0
enriquemoya <enrique.moya08@gmail.com>
2017-06-12 17:03:10 -0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { OrganizacionComponent } from './organizacion/organizacion.component';
+import { TechComponent } from './tech/tech.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    ServiciosComponent,
+    PortafolioComponent,
+    OrganizacionComponent,
+    TechComponent,
+    ContactComponent,
+    FooterComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/assets/scripts/new-age.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict"; // Start of use strict
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('body a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 200
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('nav.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery); // End of use strict
